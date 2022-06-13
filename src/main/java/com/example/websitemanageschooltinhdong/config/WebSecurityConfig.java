d41fb5e4b717ccf747d470d89f02d0fb8421284b
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-06-13 16:06:34 +0700

    update api diem

--- src/main/java/com/example/websitemanageschooltinhdong/config/WebSecurityConfig.java
+++ src/main/java/com/example/websitemanageschooltinhdong/config/WebSecurityConfig.java
@@ -93,7 +93,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/api/baiGiang/update").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/baiGiang/update").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/baiGiang/delete/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/api/diem").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/diem").hasRole("STUDENT")
                 .antMatchers(HttpMethod.PUT, "/api/diem/hocSinh").hasAnyRole("TEACHER","ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/questions/create").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/questions/chude").permitAll()
