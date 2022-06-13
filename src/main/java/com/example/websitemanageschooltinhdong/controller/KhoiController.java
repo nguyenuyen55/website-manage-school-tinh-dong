65aff3bae9501890e7bed051752ce543d2acd3e4
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-06-14 02:02:10 +0700

    update api thoi khoa bieu

--- src/main/java/com/example/websitemanageschooltinhdong/controller/KhoiController.java
+++ src/main/java/com/example/websitemanageschooltinhdong/controller/KhoiController.java
@@ -2,6 +2,7 @@ package com.example.websitemanageschooltinhdong.controller;
 
 import com.example.websitemanageschooltinhdong.domain.BaiGiang;
 import com.example.websitemanageschooltinhdong.domain.Khoi;
+import com.example.websitemanageschooltinhdong.dto.response.KhoiDTO;
 import com.example.websitemanageschooltinhdong.repository.KhoiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,11 +20,18 @@ public class KhoiController {
     @Autowired
     KhoiRepository khoiRepository;
     @GetMapping("")
-    public ResponseEntity<List<Khoi>> listResponseEntity(){
+    public ResponseEntity<List<KhoiDTO>> listResponseEntity(){
+        List<KhoiDTO> khoiDTOSs = new ArrayList<>();
         List<Khoi> khois = khoiRepository.findAll();
         if (khois.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(khois, HttpStatus.OK);
+        for(Khoi khoi : khois){
+            KhoiDTO  khoiDTO = new KhoiDTO();
+            khoiDTO.setId(khoi.getId());
+            khoiDTO.setTen(khoi.getTen());
+                 khoiDTOSs.add(khoiDTO);
+        }
+        return new ResponseEntity<>(khoiDTOSs, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/websitemanageschooltinhdong/dto/response/KhoiDTO.java
@@ -0,0 +1,13 @@
+package com.example.websitemanageschooltinhdong.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class KhoiDTO {
+private int id;
+private String ten;
+}
